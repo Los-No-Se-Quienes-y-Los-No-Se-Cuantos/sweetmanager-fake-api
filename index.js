aa30f937c483bb4b09329c754756235d36b10cce
Ryzeon <alex.ramon2004@gmail.com>
2024-05-01 19:22:08 -0500

    fix: cros

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-const jsonServer = require('json-server')
-const data = require('./db.json')
-const routes = require('./routes.json')
+// server.js
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const cors = require("cors");
 
-const router = jsonServer.router(data) // Express router
-const server = jsonServer.create()       // Express server
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-server.use(jsonServer.rewriter(routes))
-server.use(router)
-
-// Avoid CORS issue
-server.use( (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+server.use(middlewares);
+server.use(router);
+server.use(
+    cors({
+        origin: true,
+        credentials: true,
+        preflightContinue: false,
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    })
+);
+server.options("*", cors());
+server.listen(3001, () => {
+    console.log("JSON Server is running");
 });
-
-server.listen(3001, () => { // 监听的端口
-    console.log('JSON Server is running')
-})
